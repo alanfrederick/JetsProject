de27c8e778b141501de52ccbec3aaac6138cf751
alanfrederick <alan@alanfrederick.com>
2018-03-10 12:45:43 -0700

    add fly method for menu

--- src/JetsApplication.java
+++ src/JetsApplication.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class JetsApplication {
 
   public static void main(String[] args) {
+    flyTheJets();
 
-
-    
   }
 
   public static void planeStatsMenu() {
@@ -24,11 +23,32 @@ public class JetsApplication {
       int fleetSelection = input.nextInt();
       selectPlane(fleetSelection - 1, jets);
     }
+  }
+
+  // fly the jets method
+  public static void flyTheJets() {
+    Scanner input = new Scanner(System.in);
+    AirField airField = new AirField();
+    Jet[] jets = airField.getJets();
+    System.out.println("Select an available Jet to see if fly!");
+    while (true) {
+      for (int i = 0; i < jets.length; i++) {
+        if (jets[i] != null) {
+          System.out.println(i + 1 + ": " + jets[i].getModel());
+        }
+      }
+      // select jet with method, accounting for 0->1 for selections
+      int fleetSelection = input.nextInt();
+      selectPlaneToFly(fleetSelection - 1, jets);
+    }
 
   }
 
   public static void selectPlane(int selection, Jet[] jetsArry) {
     jetsArry[selection].getPlaneStats();
   }
-
+  
+ public static void selectPlaneToFly(int selection, Jet[] jetsArry) {
+   jetsArry[selection].fly();
+ }
 }

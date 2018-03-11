16f88025e1c9812b7967c2cdaad29b8c769a7856
alanfrederick <alan@alanfrederick.com>
2018-03-11 13:00:10 -0600

    update menu add menu methods

--- src/JetsApplication.java
+++ src/JetsApplication.java
@@ -1,10 +1,64 @@
 import java.util.Scanner;
 
 public class JetsApplication {
-
+  // main calls main menu method which drives the program
   public static void main(String[] args) {
-    flyTheJets();
+    mainMenu();
+  }
 
+  // main menu method, providing a menu and options for user
+  public static void mainMenu() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("/*****************************************/");
+    System.out.println("  Welcome to the Skill Distillery Airport.\n     Please select a menu option below.");
+    System.out.println("/*****************************************/");
+    System.out.println("1. List available planes in the Airport.");
+    System.out.println("2. Fly a Jet!");
+    System.out.println("3. Add a jet to the Aiport.");
+    System.out.println("4. Display the fastest jet in the Airport.");
+    System.out.println("5. Diplay the jet with the furthest range.");
+    System.out.println("0. Exit the Skill Distillery Airport.");
+    String menuSelection = input.next();
+
+    // main menu selections
+    while (menuSelection != "") {
+      if (menuSelection.equals("1")) {
+        planeStatsMenu();
+      } else if (menuSelection.equals("2")) {
+        flyTheJets();
+
+      } else if (menuSelection.equals("3")) {
+
+      } else if (menuSelection.equals("4")) {
+
+      } else if (menuSelection.equals("5")) {
+
+      }
+
+      else if (menuSelection.equals("0")) {
+        System.out.println("Have a great day!");
+        System.exit(0);
+
+        // repeat menu if user provides invalid input
+      } else {
+        System.out.println("1. List available planes in the Airport.");
+        System.out.println("2. Fly a Jet!");
+        System.out.println("3. Add a jet to the Aiport.");
+        System.out.println("4. Display the fastest jet in the Airport.");
+        System.out.println("5. Diplay the jet with the furthest range.");
+        System.out.println("0. Exit the Skill Distillery Airport.");
+        menuSelection = input.next();
+      }
+    }
+
+  }
+  
+  public static void getFastestPlane() {
+    
+  }
+  
+  public static void getFurthestRangePlane() {
+    
   }
 
   public static void planeStatsMenu() {
@@ -47,8 +101,8 @@ public class JetsApplication {
   public static void selectPlane(int selection, Jet[] jetsArry) {
     jetsArry[selection].getPlaneStats();
   }
-  
- public static void selectPlaneToFly(int selection, Jet[] jetsArry) {
-   jetsArry[selection].fly();
- }
+
+  public static void selectPlaneToFly(int selection, Jet[] jetsArry) {
+    jetsArry[selection].fly();
+  }
 }
